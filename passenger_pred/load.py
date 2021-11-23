f758ca3c58c8b7b6d8d46b5843713cc9f3ebac11
Lars Kouwenhoven <31851200+larskouwenhoven@users.noreply.github.com>
2021-11-22 22:30:53 -0500

    Import son

--- passenger_pred/load.py
+++ passenger_pred/load.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import numpy as np
 from datetime import date, timedelta, datetime
+import json
 
 def load_data_range(
     start_date, end_date, route, api_url='http://api.buswatcher.org/api/v2'
