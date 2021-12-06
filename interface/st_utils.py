41950688518588f12915f8ee742331e40204892b
Anton Abilov <anton.abilov@gmail.com>
2021-12-06 18:00:35 -0500

    Weather demo performance: Use Plotly charts and add caching

--- interface/st_utils.py
+++ interface/st_utils.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import os
 
+from run_experiment import load_pickled_experiment
+
 SAVED_EXPERIMENT_DIR = "saved_experiments/"
 DATA_DIR = "data/streamlit/"
 
@@ -16,3 +18,10 @@ def load_bus_segment_data(route, processed=True):
         st.error(f"Failed to load data for route {route}: {e}")
         return
     return df_route
+
+@st.cache(allow_output_mutation=True)
+def st_load_experiment(name):
+    return load_pickled_experiment(os.path.join(
+        SAVED_EXPERIMENT_DIR,
+        name
+    ))
